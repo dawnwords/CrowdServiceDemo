ac88ffaaa4132e7fbe81cd02cd0cdec353897781
Gu Jingxiao <gujingxiao@gmail.com>
2015-03-03 20:03:44 +0800

    + send CompleteWapper when response insertion succeeds

--- src/edu/fudan/se/agent/behaviour/ReceiveResponseBehaviour.java
+++ src/edu/fudan/se/agent/behaviour/ReceiveResponseBehaviour.java
@@ -1,21 +1,40 @@
 package edu.fudan.se.agent.behaviour;
 
-import jade.core.AID;
-import edu.fudan.se.agent.ConversationType;
-import edu.fudan.se.crowdservice.wrapper.ResponseWrapper;
-import edu.fudan.se.dbopration.InsertResponseOperator;
+import edu.fudan.se.crowdservice.wrapper.*;
+import edu.fudan.se.dbopration.UpdateResponseOperator;
 import edu.fudan.se.util.XMLUtil;
+import jade.core.AID;
+import jade.lang.acl.ACLMessage;
+
+import java.io.IOException;
 
 public class ReceiveResponseBehaviour extends
-		MessageReceivingBehaviour<ResponseWrapper> {
+        MessageReceivingBehaviour<ResponseWrapper> {
 
-	public ReceiveResponseBehaviour() {
-		super(ConversationType.RESPONSE);
-	}
+    public ReceiveResponseBehaviour() {
+        super(ConversationType.RESPONSE);
+    }
 
-	@Override
-	protected void handleMessage(AID sender, ResponseWrapper content) {
-		new InsertResponseOperator(sender.getLocalName(), content.taskId,
-				XMLUtil.obj2XML(content.keyValueHolders)).getResult();
-	}
+    @Override
+    protected void handleMessage(AID sender, ResponseWrapper content) {
+        Boolean success = new UpdateResponseOperator(sender.getLocalName(), content.taskId,
+                XMLUtil.obj2XML(content.keyValueHolders)).getResult();
+        if (success != null && success) {
+            sendMessage(sender, ConversationType.COMPLETE.name(), new CompleteWrapper(content.taskId));
+        } else {
+            sendMessage(sender, ConversationType.REFUSE.name(), new RefuseWrapper(content.taskId, RefuseWrapper.Reason.OFFER_OUT_OF_DATE));
+        }
+    }
+
+    private void sendMessage(AID sender, String name, Wrapper wrapper) {
+        ACLMessage aclMsg = new ACLMessage(ACLMessage.INFORM);
+        aclMsg.setConversationId(name);
+        aclMsg.addReceiver(sender);
+        try {
+            aclMsg.setContentObject(wrapper);
+            myAgent.send(aclMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
